2ee3b228abf4c6aadfc6d9b8ba0d1d433f515245
iimagii <mag_1@ukr.net>
2022-08-08 17:44:34 +0300

    add basic functionality

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,37 @@
 <template>
-   <div>работает</div>
+   <div>
+      <div>
+         <button @click="addLike">Like</button>
+         <button @click="addDisLike">Dislike</button>
+      </div>
+
+      <div>
+         Количество лайков:<strong>{{ likes }}</strong>
+      </div>
+      <div>
+         Количество дизлайков:<strong>{{ dislikes }}</strong>
+      </div>
+   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+   data() {
+      return {
+         likes: 0,
+         dislikes: 0,
+      }
+   },
+   methods: {
+      addLike() {
+         this.likes += 1
+      },
+      addDisLike() {
+         this.dislikes += 1
+      },
+   },
+}
+</script>
 
 <style></style>
 
