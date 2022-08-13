c250ea859c8218f107043fe01e1b52fb7a4282f6
iimagii <mag_1@ukr.net>
2022-08-13 16:38:45 +0300

    added sorting and animation to the project

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
             <PostForm @create="createPost" />
          </MyDialog>
 
-         <PostList :posts="posts" @remove="removePost" v-if="!isPostsLoading" />
+         <PostList :posts="sortedPosts" @remove="removePost" v-if="!isPostsLoading" />
          <div v-else style="color: red">Идет загрузка...</div>
       </div>
    </div>
@@ -78,8 +78,14 @@ export default {
          }
       },
    },
+
    mounted() {
       this.fetchPosts()
+   },
+   computed: {
+      sortedPosts() {
+         return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+      },
    },
 }
 </script>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,7 +1,7 @@
 <template>
    <div v-show="posts.length > 0">
       <h3>Список пользователей</h3>
-      <PostItem v-for="post in posts" :post="post" :key="post.id" @remove="$emit('remove', post)"></PostItem>
+      <transition-group name="post-list"> <PostItem v-for="post in posts" :post="post" :key="post.id" @remove="$emit('remove', post)"></PostItem> </transition-group>
    </div>
    <h2 v-show="posts.length === 0" style="color: red">Нет сообщений</h2>
 </template>
@@ -19,4 +19,20 @@ export default {
    },
 }
 </script>
-<style></style>
+<style>
+.post-list-item {
+   display: inline-block;
+   margin-right: 10px;
+}
+
+.post-list-enter-active,
+.post-list-leave-active {
+   transition: all 1s ease;
+}
+
+.post-list-enter-from,
+.post-list-leave-to {
+   opacity: 0;
+   transform: translateY(30px);
+}
+</style>
